e9add220846ba469e22c289354d1fbd2dfcdbc49
Foysal-Munsy <bdfoysal555@gmail.com>
2023-06-17 14:28:39 +0600

    week 03 Day 02 update

---
+++ Week-03/Day 02/wheres the bishop.cpp
@@ -0,0 +1,67 @@
+/********************
+*   -------------   *
+*   Author:Foysal   *
+*   -------------   *
+********************/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+#define     nl '\n'
+#define     PI 3.14159265358979323846
+#define     f1(i,n)     for(int i=0;i<n;i++)
+#define     f2(j,n)     for(int j=0;j<n;j++)
+#define     in_range(i,x,y) for(int i=x;i<=y;i++)
+#define     in_range_back(i,x,y) for(int i=y;i>=x;i--)
+#define     cin_array(x,o,n) for(int i=o;i<=n;i++){cin>>x[i];}
+#define     cout_array(x,o,n) for(int i=o;i<=n;i++){cout<<x[i]<<nl;}cout<<nl;
+#define     Code    ios_base::sync_with_stdio(false);
+#define     By      cin.tie(nullptr);
+#define     Foysal  cout.tie(NULL);
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
+bool is_palindrome(int n){string str = to_string(n);for(int i=0,j=str.size()-1 ; i<=j ; i++,j--){if(str[i]!=str[j]) return false;}return true;}
+
+
+void test()
+{
+        cout << nl;
+        string s[9]; cin_array(s,0,7);
+        int row,col;
+        in_range(i,0,7)
+        {
+            in_range(j,0,7)
+            {
+                if(s[i-1][j+1]=='#' && s[i-1][j-1]=='#' &&s [i+1][j+1]=='#' && s[i+1][j-1]=='#')
+                {
+                    row=i+1; col=j+1;
+                }
+            }
+        }
+        cout<<row<<" "<<col<<endl;
+}
+int main()
+{
+    Code By Foysal
+    /*----------*/
+    ll t; cin>>t;
+    while(t--)
+    {
+        test();
+    }
+
+
+    return 0;
+}
