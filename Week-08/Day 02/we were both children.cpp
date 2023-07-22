4c04d8ea5ed6253dc804ef18acc97ab36b5f5110
Foysal-Munsy <bdfoysal555@gmail.com>
2023-07-22 22:41:27 +0600

    Day 02

---
+++ Week-08/Day 02/we were both children.cpp
@@ -0,0 +1,87 @@
+/********************
+*   -------------   *
+*   Author:Foysal   *
+*   -------------   *
+********************/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long long int lli;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+#define     nl '\n'
+#define     PI 3.14159265358979323846
+#define     f1(i,n)     for(int i=0;i<n;i++)
+#define     f2(j,n)     for(int j=0;j<n;j++)
+#define     in_range(i,x,y) for(int i=x;i<=y;i++)
+#define     in_range_back(i,x,y) for(int i=y;i>=x;i--)
+#define     yes    cout<<"YES"<<nl;
+#define     Yes    cout<<"Yes"<<nl;
+#define     no     cout<<"NO"<<nl;
+#define     No     cout<<"No"<<nl;
+#define     cin_array(x,o,n) for(int i=o;i<=n;i++){cin>>x[i];}
+#define     cout_array(x,o,n) for(int i=o;i<=n;i++){cout<<x[i]<<" ";}cout<<nl;
+#define     Code    ios_base::sync_with_stdio(false);
+#define     By      cin.tie(nullptr);
+#define     Foysal  cout.tie(NULL);
+
+void solve()
+{
+    ll n;   cin >> n;
+    vl v(n); 
+    for(ll i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    // cout_array(v,0,n-1);
+    vl freq(n+2,0);
+    for(ll i=0; i<n; i++)
+    {
+        if(v[i]>n)continue;
+        freq[v[i]]++;
+    }
+    ll ans = 0;
+    for (ll i = 1; i <= n; i++)
+    {
+        ll curr = 0;
+        for (ll x = 1; x*x <= i; x++)
+        {
+            if(i%x==0)
+            {
+                curr+=freq[x];
+                if(x*x != i)
+                {
+                    curr += freq[i/x];
+                }
+            }
+        }
+        ans = max(ans,curr);
+    }
+    cout << ans << nl;
+    
+}
+int main()
+{
+    Code By Foysal
+    /*----------*/
+    ll t=1; 
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+
+
+    return 0;
+}
