9e8eda35c21e54e258f49acf6f02b699e9031114
Foysal-Munsy <bdfoysal555@gmail.com>
2023-08-17 15:48:15 +0600

    TRY

--- Week-11/Day 06/demo.cpp
+++ Week-11/Day 06/demo.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 int main()
 {
-    cout << "HELLO\n";
+    //cout << "HELLO\n";
     vector<long long> v;
     for(int i=0; i<5; i++)
     {
         int x;  cin >> x;
         v.push_back(x);
     }
-    for(auto i:v) cout << i << " ";
+     
+    for(int i=0;i<5; i++) cout << v[i] << " ";
     cout << endl;
     return 0;
 }
