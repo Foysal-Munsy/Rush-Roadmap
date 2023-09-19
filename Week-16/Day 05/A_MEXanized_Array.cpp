73b2e709b841befef65bf8793282d8c4cb58846f
Foysal-Munsy <bdfoysal555@gmail.com>
2023-09-19 23:52:50 +0600

    UPDATE

---
+++ Week-16/Day 05/A_MEXanized_Array.cpp
@@ -0,0 +1,31 @@
+/*----------------------------*
+ *   -----------------------   *
+ *    Author: Foysal Munsy     *
+ *    Date: 2023/09/19         *
+ *    Time: 23:51:29           *
+ *   -----------------------   *
+ *-----------------------------*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define nl '\n'
+
+void solve()
+{
+}
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    /*-----Code By Foysal-----*/
+    ll cs = 1;
+    ll t = 1;
+    cin >> t;
+    for (; t--;)
+    {
+        // cout << "#case " << cs << " :\n";
+        solve();
+        cs++;
+    }
+    return 0;
+}
