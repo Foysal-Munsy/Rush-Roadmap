a66f90cb74bf4c2c748286e5ed88e2578c25d6c3
Foysal-Munsy <bdfoysal555@gmail.com>
2023-10-02 11:55:06 +0600

    Update

--- Week-18 (Alone)/Day 04/Operations with string.cpp
+++ Week-18 (Alone)/Day 04/Operations with string.cpp
@@ -56,14 +56,8 @@ signed main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     /*-----Code By Foysal-----*/
-    ll cs = 1;
-    ll t = 1;
-    // cin >> t;
-    for (; t--;)
-    {
-        // cout << "#case " << cs << " :\n";
-        solve();
-        cs++;
-    }
+
+    solve();
+
     return 0;
 }
